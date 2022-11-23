a0bc4b7d4897ccb5262a9a0fbe15915a2f3eedd0
Matt Burke <spraints@gmail.com>
2022-11-23 09:24:12 -0500

    Maybe don't store users every time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,33 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?
-  def logged_in?
-    rand > 0.5
+  def current_user
+    @current_user ||= get_current_user
+  end
+
+  private
+
+  def get_current_user
+    get_current_user_from_session ||
+      get_current_user_from_remember_me_token ||
+      create_current_user
+  end
+
+  def get_current_user_from_session
+    User.where(id: session[:current_user_id]).first
+  end
+
+  def get_current_user_from_remember_me_token
+    token = RememberMeToken.where(token: cookies[:remember_me]).first
+    token&.user
+  end
+
+  def create_current_user
+    # todo - don't make a new user every time, just generate an id. when the
+    # user does something, then store the user. Or maybe just store the ID.
+    user = User.create!
+    token = user.remember_me_tokens.create!
+    # clear the session?
+    session[:current_user_id] = user.id
+    cookies.permanent.signed[:remember_me] = token.token
+    user
   end
 end
--- app/models/remember_me_token.rb
+++ app/models/remember_me_token.rb
@@ -1,3 +1,9 @@
 class RememberMeToken < ApplicationRecord
   belongs_to :user
+
+  before_create :generate_token!
+
+  def generate_token!
+    raise "todo"
+  end
 end
